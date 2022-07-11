63822456605d862f1759f031487a0980422b65ea
Tony Herbert <tonyherbert22@gmail.com>
2022-07-11 16:52:38 -0500

    update image

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const aliens = {
         interestingFact:
             "Have Ability to shape their bodies into whatever physical form they chose to repair themselves whenever damaged, including rejoining separations of their bodies. ",
         notableMembers: ["Yaphit"],
-        image: "https://static.wikia.nocookie.net/orville/images/2/27/EMercer.jpg/revision/latest?cb=20171004230231",
+        image: "https://static.wikia.nocookie.net/orville/images/9/9c/Orville-norm-macdonald.jpg/revision/latest/scale-to-width-down/1000?cb=20170922163525",
     },
     kaylon: {
         speciesName: "Kaylon",
@@ -28,7 +28,7 @@ const aliens = {
         interestingFact:
             "Kaylons were constructed by a biological species native to Kaylon 1, known as 'Builders' Eventually, leading to their demise.",
         notableMembers: ["Isaac"],
-        image: "https://static.wikia.nocookie.net/orville/images/2/27/EMercer.jpg/revision/latest?cb=20171004230231",
+        image: "https://static.wikia.nocookie.net/orville/images/e/ef/Isaac2.jpg/revision/latest/scale-to-width-down/1000?cb=20171002233754",
     },
     krill: {
         speciesName: "Krill",
@@ -38,7 +38,7 @@ const aliens = {
         interestingFact:
             "In Krill society, sunlight is a symbol of death, while rain and darkness are symbols of good.",
         notableMembers: ["Anaya", "Korin", "Teleya", "Haros", "Sazeron"],
-        image: "https://static.wikia.nocookie.net/orville/images/2/27/EMercer.jpg/revision/latest?cb=20171004230231",
+        image: "https://static.wikia.nocookie.net/orville/images/d/d2/Krill-0.png/revision/latest?cb=20170920225634",
     },
     moclan: {
         speciesName: "Moclan",
@@ -48,7 +48,7 @@ const aliens = {
         interestingFact:
             "Moclans reproduced by laying eggs. The Moclan government almost always forcibly changed the sex of a female to male shortly after birth.",
         notableMembers: ["Bortus", "Klyden", "Mersa", "Topa"],
-        image: "https://static.wikia.nocookie.net/orville/images/2/27/EMercer.jpg/revision/latest?cb=20171004230231",
+        image: "https://static.wikia.nocookie.net/orville/images/2/2f/Bortus2.jpg/revision/latest/scale-to-width-down/1000?cb=20171006200215",
     },
     xelayan: {
         speciesName: "Xelayan",
@@ -58,7 +58,7 @@ const aliens = {
         interestingFact:
             "Due to the comparatively high gravity of their home planet, Xelayans enjoyed exceptional strength outside of Xelaya.",
         notableMembers: ["Alara Kitan", "Cambis Borrin", "Serris"],
-        image: "https://static.wikia.nocookie.net/orville/images/2/27/EMercer.jpg/revision/latest?cb=20171004230231",
+        image: "https://static.wikia.nocookie.net/orville/images/9/90/Alara.jpg/revision/latest/scale-to-width-down/1000?cb=20171002233437",
     },
 };
 app.get("/", (request, response) => {
